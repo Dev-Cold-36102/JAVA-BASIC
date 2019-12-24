4f0aa6a42f92bee57ffc2eac969dbb62b7061307
Dev.Cold <devcolduet@gmail.com>
2019-12-24 15:47:38 +0700

    add file

--- src/PrimeNumber_20.java
+++ src/PrimeNumber_20.java
@@ -5,23 +5,22 @@ public class PrimeNumber_20 {
         Scanner sc = new Scanner(System.in);
         System.out.print("please enter the number:");
         int number = sc.nextInt();
-        System.out.println(number+" prime number first:");
+        System.out.println(number + " prime number first:");
 
-        int amout=0;
+        int amout = 0;
 
-            for (int i = 2; amout<=number ; i++) {
-                int count = 0;
-                for (int j = 1; j <= Math.sqrt(i); j++) {
-                    if (i % j == 0) {
-                        count++;
-                    }
-                }
-                if (count == 1) {
-                    amout++;
-                    System.out.print(i+" ");
+        for (int i = 2; amout < number; i++) {
+            int count = 0;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    count++;
                 }
             }
-
+            if (count == 1) {
+                amout++;
+                System.out.print(i + " ");
+            }
+        }
 
 
     }
