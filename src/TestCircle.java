915ab08e9cd35977d0063075818dd9f518c060f3
Dev.Cold <devcolduet@gmail.com>
2020-01-03 11:54:48 +0700

    add file

--- src/TestCircle.java
+++ src/TestCircle.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
 public class TestCircle {
-    static class Circle {
-        private double radius = 1.0;
-        private String color = "red";
-        final double PI = 3.14;
 
-        Circle() {
-
-        }
-
-        Circle(double r) {
-
-        }
-
-        private double getRadius() {
-            return radius;
-        }
-
-        public double getArea() {
-            return Math.pow(radius, 2) * PI;
-
-        }
-    }
 
     public static void main(String[] args) {
         Circle circle = new Circle();
         System.out.println("area=" + circle.getArea());
+    }
+}
+ class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+    final double PI = 3.14;
+
+    Circle() {
+
+    }
+
+    Circle(double r) {
+
+    }
+
+    private double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.pow(radius, 2) * PI;
+
     }
 }
